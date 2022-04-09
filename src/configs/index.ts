326e736a7349d35a70b6001f224a7c11e26476e5
David Stefani Prado <daviprado1995@gmail.com>
2022-04-09 16:06:43 -0300

    alterações feitas para adicionar o cache e eleições

--- src/configs/index.ts
+++ src/configs/index.ts
@@ -2,7 +2,7 @@ export const configs = {
     apis: {
         correios:process.env.REACT_APP_CEP_URL || 'http://cep.republicavirtual.com.br/web_cep.php',
         starWars:process.env.REACT_APP_STAR_WARS_BASE_URL || 'https://star-wars-api-unifacef.herokuapp.com',
-        github:process.env.REACT_APP_GITHUB_URL,
+        github: process.env.REACT_APP_GITHUB_URL || 'https://api.github.com',
     
     },
     sentry: process.env.REACT_APP_SENTRY_DSN || 'https://e3809b7cca6a43238284a3056ad727ba@o1196607.ingest.sentry.io/6319616',
