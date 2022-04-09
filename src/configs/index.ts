4da0f4d9dd56cee84e822d2832d981546b1cafc1
David Stefani Prado <daviprado1995@gmail.com>
2022-04-09 14:16:40 -0300

    adicionar as funções de register e cep

--- src/configs/index.ts
+++ src/configs/index.ts
@@ -1,8 +1,9 @@
 export const configs = {
     apis: {
-        cep:process.env.REACT_APP_CEP_URL,
+        correios:process.env.REACT_APP_CEP_URL || 'http://cep.republicavirtual.com.br/web_cep.php',
         starWars:process.env.REACT_APP_STAR_WARS_BASE_URL || 'https://star-wars-api-unifacef.herokuapp.com',
         github:process.env.REACT_APP_GITHUB_URL,
+    
     },
     sentry: process.env.REACT_APP_SENTRY_DSN || 'https://e3809b7cca6a43238284a3056ad727ba@o1196607.ingest.sentry.io/6319616',
 
